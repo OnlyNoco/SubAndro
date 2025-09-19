fbb633c036611ce0f080e0c615878c38db10ed3d
OnlyNoco <abidabdullah.cloud@gmail.com>
2025-09-19 22:53:17 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,40 +1,8 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id("com.android.application") version "8.1.4" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
 
-android {
-    namespace = "com.onlynoco.fansubcreator"
-    compileSdk = 34
-    defaultConfig {
-        applicationId = "com.onlynoco.fansubcreator"
-        minSdk = 21
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
-
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.10")
-    implementation("com.arthenica:ffmpeg-kit-full:6.0")
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.10.0")
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
